a09bee72ce8545f4a56cab7516464b461f212452
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-03-04 15:41:34 -0800

    add encrypting password function

--- server/index.js
+++ server/index.js
@@ -1,6 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const User = require("./src/models/user");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+
+// Connecting DB
+try {
+  mongoose.connect(process.env.MONGO_URI);
+  console.log("connecting to DB");
+} catch (err) {
+  console.log(err);
+}
+
+app.post("/register", async (req, res) => {
+  const password = req.body.password;
+  try {
+    // Encrypting the password
+    req.body.password = CryptoJS.AES.encrypt(
+      "password",
+      process.env.SECRET_KEY
+    );
+
+    // Creating a new user
+    const user = await User.create(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
