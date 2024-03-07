1e63313a21ce9195a50eee0c5d2a230922dc9b3a
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-03-06 20:29:00 -0800

    add function to get one memo

--- server/routes/memoRoute.js
+++ server/routes/memoRoute.js
@@ -4,4 +4,10 @@ const tokenHandler = require("../src/handlers/tokenHandler");
 
 router.post("/", tokenHandler.verifyToken, memoController.create);
 
+// get all memos from a use logged in
+router.get("/", tokenHandler.verifyToken, memoController.getALL);
+
+// get one memos from a use logged in
+router.get("/:memoId", tokenHandler.verifyToken, memoController.getOne);
+
 module.exports = router;
--- server/src/controllers/memoController.js
+++ server/src/controllers/memoController.js
@@ -12,3 +12,23 @@ exports.create = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+exports.getALL = async (req, res) => {
+  try {
+    const memos = await Memo.find({ user: req.user._id }).sort("-position");
+    res.status(200).json(memos);
+  } catch {
+    res.status(500).json(err);
+  }
+};
+
+exports.getOne = async (req, res) => {
+  const { memoId } = req.params;
+  try {
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
+    if (!memo) return res.status(404).json({ message: "Memo not found" });
+    res.status(200).json(memo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
